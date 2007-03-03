2d72b7b9612cdcdb34be746c4e8559ba7f0ea512
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2007-03-03 15:26:57 +0000

    Restaurant Redirect object will take care of redirecting people who go to restaurants who have new URLs
    git-svn-id: http://svn.reviewsby.us/trunk@92 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

---
+++ lib/model/RestaurantRedirect.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'restaurant_redirect' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class RestaurantRedirect extends BaseRestaurantRedirect
+{
+}
