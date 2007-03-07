7e7cb33c0f4edcb0c2ff008f176d80adf72d04a2
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2007-03-07 03:46:46 +0000

    For some reason the __toString() functions don't operate the same way on different servers.  So I rebuild the brif function to explicitly use __toString().
    git-svn-id: http://svn.reviewsby.us/trunk@101 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

--- apps/frontend/modules/location/templates/_location.php
+++ apps/frontend/modules/location/templates/_location.php
@@ -2,6 +2,6 @@
 <p>
 <?php echo brif($location->getAddress()) ?>
 <?php echo brif(textif($location->getCity(), ', ') . textif($location->getState(), ' ') . $location->getZip()) ?>
-<?php echo brif($location->getCountry()) ?>
+<?php echo brif($location->getCountry(),$location->getCountry()->__toString()) ?>
 <label for="phone">Phone:</label> <span id="phone"><?php echo format_phone($location->getPhone()) ?></span>
 </p>
