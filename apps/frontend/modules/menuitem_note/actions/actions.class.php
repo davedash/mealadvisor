255d2d30e19542ecd45733903a5c290c988620f5
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2007-09-25 03:44:14 +0000

    This should have been here...
    git-svn-id: http://svn.reviewsby.us/trunk@167 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

---
+++ apps/frontend/modules/menuitem_note/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * menuitem_note actions.
+ *
+ * @package    reviewsby.us
+ * @subpackage menuitem_note
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class menuitem_noteActions extends automenuitem_noteActions
+{
+}
