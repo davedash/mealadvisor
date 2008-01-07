0bab0c8a9351b7685e874bdeb33cff645d88212b
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2008-01-07 00:08:14 +0000

    Accesible form
    git-svn-id: http://svn.reviewsby.us/trunk@179 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

--- apps/frontend/modules/default/templates/indexSuccess.php
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -1,9 +1,50 @@
+<?php use_helper('AccessibleForm');?>
 <div id="recent_dishes">
 <h2>Recent Dishes</h2>
 <?php include_component('menuitem', 'feature') ?>
 </div>
-<div id="review_form_area">
+<div id="review_form">
   <h2>&#8230;so what have you been eating?</h2>
   
-  
+  <?php echo accessible_form_tag('#') ?>
+  <fieldset>
+    <legend>Delivery Details</legend>
+    <ol>
+      <li>
+        <label for="name">Name<em>*</em></label>
+        <input id="name" />
+      </li>
+      <li>
+        <label for="address1">Address<em>*</em></label>
+        <input id="address1" />
+      </li>
+      <li>
+        <label for="address2">Address 2</label>
+        <input id="address2" />
+      </li>
+      <li>
+        <label for="town-city">Town/City</label>
+        <input id="town-city" />
+      </li>
+      <li>
+        <label for="county">County<em>*</em></label>
+        <input id="county" />
+      </li>
+      <li>
+        <label for="postcode">Postcode<em>*</em></label>
+        <input id="postcode" />
+      </li>
+      <li>
+        <fieldset>
+          <legend>Is this address also your invoice »
+  address?<em>*</em></legend>
+          <label><input type="radio" »
+  name="invoice-address" /> Yes</label>
+          <label><input type="radio" »
+  name="invoice-address" /> No</label>
+        </fieldset>
+      </li>
+    </ol>
+  </fieldset>
+    </form>
 </div>
