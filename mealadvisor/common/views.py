a1200b80429b2ff9f976ea5de7ff29540ebd9345
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2008-02-05 04:59:24 +0000

    Moved index.html to common/ creeted search view
    git-svn-id: http://svn.reviewsby.us/trunk@208 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

--- mealadvisor/common/views.py
+++ mealadvisor/common/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response, get_object_or_404
 
 def home(request):
-  return render_to_response("index.html")
+  return render_to_response("common/index.html")
   
+def search(request):
+  # get results from model
+  return render_to_response("common/search.html")
