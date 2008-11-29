e07796a30ba4675be929ac691c882a5ac1882dd5
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2008-11-29 08:43:32 +0000

    Created a restaurant review form.
    git-svn-id: http://svn.reviewsby.us/trunk@273 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

---
+++ mealadvisor/restaurant/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+STARS = TOPIC_CHOICES = (
+    ('',''),
+    ('1', '*'),
+    ('2', '**'),
+    ('3', '***'),
+    ('4', '****'),
+    ('5', '*****'),
+)
+
+class ReviewForm(forms.Form):
+    # rating = forms.ChoiceField(choices=STARS, error_messages={'required': 'Please choose a star rating'})
+    note   = forms.CharField(widget=forms.Textarea(),error_messages={'required': 'Please type in your message'})
+    
