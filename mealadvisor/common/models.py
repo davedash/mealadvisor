ce19f7ba6905c4947c9b5725e9b111d419e56f9c
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2008-12-08 02:43:32 +0000

    Cleaned up the common models.  Removed unused comments.
    git-svn-id: http://svn.reviewsby.us/trunk@286 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

--- mealadvisor/common/models.py
+++ mealadvisor/common/models.py
@@ -1,12 +1,4 @@
 # coding=utf-8
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#     * Rearrange models' order
-#     * Make sure each model has one field with primary_key=True
-# Feel free to rename the models, but don't rename db_table values or field names.
-#
-# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
-# into your database.
 
 from django.db import models
 from django.db.models import Q
@@ -115,75 +107,6 @@ class State(models.Model):
 #     weight = models.IntegerField(null=True, blank=True)
 #     class Meta:
 #         db_table = u'restaurant_search_index'
-# 
-# class RestaurantTag(models.Model):
-#     restaurant = models.ForeignKey(Restaurant)
-#     user = models.ForeignKey(Profile)
-#     created_at = models.DateTimeField(null=True, blank=True)
-#     tag = models.CharField(max_length=300, blank=True)
-#     normalized_tag = models.CharField(max_length=300)
-#     class Meta:
-#         db_table = u'restaurant_tag'
-# 
-# class SfGuardGroup(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     name = models.CharField(max_length=765)
-#     description = models.TextField()
-#     class Meta:
-#         db_table = u'sf_guard_group'
-# 
-# class SfGuardGroupPermission(models.Model):
-#     group = models.ForeignKey(SfGuardGroup)
-#     permission = models.ForeignKey(SfGuardPermission)
-#     class Meta:
-#         db_table = u'sf_guard_group_permission'
-# 
-# class SfGuardModule(models.Model):
-#     module_name = models.CharField(max_length=300, primary_key=True)
-#     class Meta:
-#         db_table = u'sf_guard_module'
-# 
-# class SfGuardPermission(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     name = models.CharField(max_length=765)
-#     module_name = models.ForeignKey(SfGuardModule, null=True, db_column='module_name', blank=True)
-#     description = models.TextField()
-#     action_name = models.CharField(max_length=300, blank=True)
-#     class Meta:
-#         db_table = u'sf_guard_permission'
-# 
-# class SfGuardRememberKey(models.Model):
-#     user = models.ForeignKey(SfGuardUser)
-#     remember_key = models.CharField(max_length=96, blank=True)
-#     ip_address = models.CharField(max_length=45, primary_key=True)
-#     created_at = models.DateTimeField(null=True, blank=True)
-#     class Meta:
-#         db_table = u'sf_guard_remember_key'
-# 
-# class SfGuardUser(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     username = models.CharField(max_length=384)
-#     algorithm = models.CharField(max_length=384)
-#     salt = models.CharField(max_length=384)
-#     password = models.CharField(max_length=384)
-#     created_at = models.DateTimeField()
-#     last_login = models.DateTimeField()
-#     is_active = models.IntegerField()
-#     is_super_admin = models.IntegerField()
-#     class Meta:
-#         db_table = u'sf_guard_user'
-# 
-# class SfGuardUserGroup(models.Model):
-#     group = models.ForeignKey(SfGuardGroup)
-#     user = models.ForeignKey(SfGuardUser)
-#     class Meta:
-#         db_table = u'sf_guard_user_group'
-# 
-# class SfGuardUserPermission(models.Model):
-#     user = models.ForeignKey(SfGuardUser)
-#     permission = models.ForeignKey(SfGuardPermission)
-#     class Meta:
-#         db_table = u'sf_guard_user_permission'
 # 
 # 
 # class YahooLocalCategory(models.Model):
