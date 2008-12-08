8b3d4ed0f307cd244c5f08eeca3e0553fe78fc07
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2008-12-08 06:25:21 +0000

    Migrator now adds id fields
    git-svn-id: http://svn.reviewsby.us/trunk@307 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

--- scripts/migrator.sh
+++ scripts/migrator.sh
@@ -24,6 +24,31 @@ DROP TABLE sf_guard_user_group;
 REPLACE INTO auth_user (id, username, password, is_active, last_login, date_joined) SELECT id, username, CONCAT(algorithm,'$', salt, '$', password), 1, last_login, created_at FROM sf_guard_user; DROP TABLE sf_guard_user;
 UPDATE auth_user SET is_staff = 1 WHERE id=8;
 UPDATE auth_user SET is_staff = 1, is_superuser=1 WHERE id=1;
+
+CREATE TABLE `restaurant_tag2` (
+  id int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `restaurant_id` int(11) NOT NULL default '0',
+  `user_id` int(11) NOT NULL default '0',
+  `created_at` datetime default NULL,
+  `tag` varchar(100) default NULL,
+  `normalized_tag` varchar(100) NOT NULL default '',
+  PRIMARY KEY  (`id`),
+  UNIQUE (restaurant_id, user_id, normalized_tag),
+  KEY `normalized_tag_index` (`normalized_tag`),
+  KEY `restaurant_tag_FI_2` (`user_id`),
+  FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `profile` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO restaurant_tag2 (user_id, restaurant_id, created_at, tag, normalized_tag) 
+SELECT user_id, restaurant_id, created_at, tag, normalized_tag FROM restaurant_tag ORDER BY created_at;
+
+DROP TABLE restaurant_tag
+ALTER TABLE restaurant_tag2 RENAME restaurant_tag
+
+alter table restaurant_search_index ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT, add primary key (id);
+
+
 SET foreign_key_checks = 1;
 " | mysql -u root rbu     
 
