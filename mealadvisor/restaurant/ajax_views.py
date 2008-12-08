250e3af35f4e0c3f45c2f077b86cd0d336e18f1b
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2008-12-08 04:22:00 +0000

    A container to hold ajax views
    git-svn-id: http://svn.reviewsby.us/trunk@291 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

---
+++ mealadvisor/restaurant/ajax_views.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from models import RestaurantTag
+from forms import TagAddForm
+from views import get_restaurant
+
+
+@login_required    
+def tags(request):
+    q = ''
+    if 'q' in request.REQUEST:
+        q = request.REQUEST['q']
+        
+    tags = RestaurantTag.objects.get_tags_for_user(request.user.get_profile(), q)
+    return HttpResponse("\n".join(tags), mimetype="text/plain")
+    
+
+@login_required
+def tag_add(request):
+    form       = TagAddForm(request.POST)
+    restaurant = get_restaurant(request.REQUEST['restaurant'])
+
+    if form.is_valid():
+        tag, created = RestaurantTag.objects.get_or_create(restaurant=restaurant, user=request.user.get_profile(), tag=request.REQUEST['tag'])
+        
+        tag.save()
+    
+    
+    return render_to_response("restaurant/tags.html", locals(), context_instance=RequestContext(request))
+    
