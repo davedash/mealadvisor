66184bd16273b0d4377ee13ff49e2d1b94b8ad98
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2009-02-19 01:54:48 +0000

    A script that will dump the pictures from the DB into the filesystem.
    git-svn-id: http://svn.reviewsby.us/trunk@368 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

---
+++ scripts/fix_images.py
@@ -0,0 +1,33 @@
+from django.core.management import setup_environ
+import sys,os
+
+sys.path.append('../mealadvisor')
+
+import mealadvisor.settings
+
+setup_environ(mealadvisor.settings)
+
+from mealadvisor.restaurant.models import MenuItemImage
+
+images = MenuItemImage.objects.all()
+
+counter = 1
+
+for img in images:
+    print counter, "of", len(images)
+    filename = 'mealadvisor/static/images/menuitems/%s.jpg'%img.md5sum
+    print filename
+    f = open(filename, 'w')
+    f.write(img.data)
+    f.close()
+    counter += 1
+    
+print "SAFE to do the following: '%s'" % """
+
+alter table menu_item_image drop data;
+alter table menu_item_image add column `image` varchar(240) NOT NULL;
+update menu_item_image set image = CONCAT('images/menuitems/',md5, '.jpg');
+alter table menu_item_image drop md5sum;
+
+
+"""
