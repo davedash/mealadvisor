027dc538414d3788bc8b96f9274cd250dd7555a6
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2009-03-07 23:27:24 +0000

    CSS is compressed on push
    git-svn-id: http://svn.reviewsby.us/trunk@417 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

--- fabfile.py
+++ fabfile.py
@@ -33,7 +33,15 @@ def staging():
     # symlink to images from /var/www/static-staging.mealadvisor/staging/images/menuitems/* new release dir
     run("rm -r %(path)s/static/images/menuitems", fail=abort)
     run("ln -s %(static_path)s/menuitems_staging %(path)s/static/images/menuitems", fail=abort)
+    
+    # upload a compressed js file to the server
+    invoke(concat_minify_js)
+    put('static/js/ma-min.js', '%(path)s/static/js')
 
+    # upload a compressed css file to the server
+    invoke(concat_minify_js)
+    put('static/css/ma-min.css', '%(path)s/static/css')
+    
     # rotate "staging" symlinks
     run('rm %(releases_path)s/staging.rollback', fail='warn')
     run('mv %(releases_path)s/staging  %(releases_path)s/staging.rollback', fail='warn')
@@ -58,7 +66,19 @@ def svn_get_version():
     output = Popen(["svn", "info", "mealadvisor"], stdout=PIPE).communicate()[0]
     return output.partition('Revision: ')[2].partition('\n')[0]
 
+def concat_minify_js():
+    # concat js
+    local('cat static/js/main.js static/js/home.js static/js/restaurant.js > static/js/ma-all.js', abort='fail')
+    # run compressor
+    # save output to ma-min.js
+    local('java -jar bin/yuicompressor-2.4.2.jar -v static/js/ma-all.js -o static/js/ma-min.js', abort='fail')
 
+def concat_minify_css():
+    # concat js
+    local('cat static/css/main.css static/css/home.css static/css/restaurant.css static/css/search.css > static/css/ma-all.css', abort='fail')
+    # run compressor
+    # save output to ma-min.js
+    local('java -jar bin/yuicompressor-2.4.2.jar -v static/css/ma-all.css -o static/css/ma-min.css', abort='fail')
 
 config.fab_hosts = ['mealadvisor.us']
 config.fab_user = 'builder'
