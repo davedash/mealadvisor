2eaae05c6b9276412bac9bd29b699f73b3d4330a
davedash <davedash@dd39a128-f71d-0410-bdc2-cd2a64f95eb5>
2009-06-22 02:42:22 +0000

    WSGI for production
    git-svn-id: http://svn.reviewsby.us/trunk@449 dd39a128-f71d-0410-bdc2-cd2a64f95eb5

---
+++ config/wsgi-prod.py
@@ -0,0 +1,16 @@
+import os
+import site
+import sys
+
+site.addsitedir('/a/mealadvisor.us/lib/python2.5/site-packages/')
+site.addsitedir('/a/mealadvisor.us/staging/')
+site.addsitedir('/a/mealadvisor.us/staging/mealadvisor')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mealadvisor.settings'
+os.environ['DJANGO_ENVIRONMENT']     = 'prod'
+
+sys.stdout = sys.stderr
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
