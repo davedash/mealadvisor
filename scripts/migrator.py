d2a347669274fd58534aad99fe0ae4939497440a
Dave Dash <dd+github@davedash.com>
2009-09-20 00:14:23 -0700

    Fixed pep8

--- scripts/migrator.py
+++ scripts/migrator.py
@@ -4,10 +4,10 @@
 
 from optparse import OptionParser
 from subprocess import *
-import os,sys
+import os
+import sys
 
 from django.core.management import setup_environ
-import sys,os
 sys.path.append('..')
 import mealadvisor.settings
 setup_environ(mealadvisor.settings)
@@ -17,34 +17,42 @@ from models.m import MenuItemImage
 
 usage = "usage: %prog [options]"
 parser = OptionParser(usage=usage)
-parser.add_option("-d", "--download", action="store_true", help="downloads database from production servers")
-parser.add_option("-T", "--notransform", action="store_false", help="does not do db import and transforms")
+parser.add_option("-d", "--download", action="store_true",
+help="downloads database from production servers")
+parser.add_option("-T", "--notransform", action="store_false",
+help="does not do db import and transforms")
 parser.add_option("--db", default='rbu', help="destination database")
 parser.add_option("--dbpass", help="db password")
-
-
 
 (options, args) = parser.parse_args()
 
 if options.download:
     print "Downloading database..."
-    # ssh root@reviewsby.us mysqldump -u migrator -pnarnians rbu --skip-opt --create-options --add-drop-table --skip-add-locks  --skip-comments --extended-insert > /tmp/db.rbu
+    # ssh root@reviewsby.us mysqldump -u migrator -pnarnians rbu --skip-opt
+    # --create-options --add-drop-table --skip-add-locks  --skip-comments
+    # --extended-insert > /tmp/db.rbu
 
-    p = Popen("ssh root@reviewsby.us mysqldump -u migrator -pnarnians rbu --skip-opt --create-options --add-drop-table --skip-add-locks  --skip-comments --extended-insert ", shell=True, stdout=PIPE)
+    p = Popen("ssh root@reviewsby.us mysqldump -u migrator -pnarnians rbu "
+    + "--skip-opt --create-options --add-drop-table --skip-add-locks "
+    + "--skip-comments --extended-insert ", shell=True, stdout=PIPE)
     dump = p.communicate()[0]
 
     f = open('/tmp/db.rbu', 'w')
     f.write(dump)
     f.close()
 
-# insert this into DB
+
 def mysql(cmds, use=True):
+    """ Insert this into DB"""
     if use:
         cmds = "use %s; " % options.db + cmds
-    p_echo  = Popen(["echo", cmds], stdout=PIPE)
+
+    p_echo    = Popen(["echo", cmds], stdout=PIPE)
     mysql_cmd = "mysql -u root"
+
     if options.dbpass:
         mysql_cmd = mysql_cmd + " -p%s"%options.dbpass
+
     p_mysql = Popen([mysql_cmd], shell=True, stdin=p_echo.stdout, stdout=PIPE)
     return p_mysql.communicate()[0]
 
@@ -78,8 +86,11 @@ DROP TABLE sf_guard_remember_key;
 DROP TABLE sf_guard_user_permission;
 DROP TABLE sf_guard_user_group;
 
-REPLACE INTO auth_user (id, username, password, is_active, last_login, date_joined)
-SELECT id, replace(username, 'http://', ''), CONCAT(algorithm,'$', salt, '$', password), 1, last_login, created_at
+REPLACE INTO
+    auth_user (id, username, password, is_active, last_login, date_joined)
+SELECT
+    id, replace(username, 'http://', ''),
+    CONCAT(algorithm,'$', salt, '$', password), 1, last_login, created_at
 FROM sf_guard_user;
 DROP TABLE sf_guard_user;
 
@@ -104,17 +115,23 @@ CREATE TABLE restaurant_tag2 (
   FOREIGN KEY (user_id) REFERENCES profile (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO restaurant_tag2 (user_id, restaurant_id, created_at, tag, normalized_tag)
-SELECT user_id, restaurant_id, created_at, tag, normalized_tag FROM restaurant_tag ORDER BY created_at;
+INSERT INTO
+    restaurant_tag2 (user_id, restaurant_id, created_at, tag, normalized_tag)
+SELECT
+    user_id, restaurant_id, created_at, tag, normalized_tag
+FROM restaurant_tag ORDER BY created_at;
 
 DROP TABLE restaurant_tag;
 ALTER TABLE restaurant_tag2 RENAME restaurant_tag;
 
-alter table restaurant_search_index ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, add primary key (id);
-alter table menuitem_tag change id id INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, add primary key (id);
-alter table menuitem_search_index ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, add primary key (id);
+ALTER TABLE restaurant_search_index
+    ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, add primary key (id);
 
-
+ALTER TABLE menuitem_tag ¨
+    CHANGE id id INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST,
+    ADD primary key (id);
+ALTER TABLE menuitem_search_index
+    ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, ADD primary key (id);
 
 SET NAMES latin1;
 ALTER TABLE restaurant MODIFY COLUMN name TEXT CHARACTER SET latin1;
@@ -125,44 +142,82 @@ ALTER TABLE restaurant MODIFY COLUMN name VARCHAR(128);
 ALTER TABLE menu_item MODIFY COLUMN name TEXT CHARACTER SET latin1;
 ALTER TABLE menu_item MODIFY COLUMN name BLOB;
 ALTER TABLE menu_item MODIFY COLUMN name TEXT CHARACTER SET utf8;
-ALTER TABLE restaurant_version MODIFY COLUMN description TEXT CHARACTER SET latin1;
-ALTER TABLE restaurant_version MODIFY COLUMN description BLOB;
-ALTER TABLE restaurant_version MODIFY COLUMN description TEXT CHARACTER SET utf8;
-ALTER TABLE restaurant_version MODIFY COLUMN html_description TEXT CHARACTER SET latin1;
+ALTER TABLE restaurant_version
+    MODIFY COLUMN description TEXT CHARACTER SET latin1;
+ALTER TABLE restaurant_version
+    MODIFY COLUMN description BLOB;
+ALTER TABLE restaurant_version
+    MODIFY COLUMN description TEXT CHARACTER SET utf8;
+ALTER TABLE restaurant_version
+    MODIFY COLUMN html_description TEXT CHARACTER SET latin1;
 ALTER TABLE restaurant_version MODIFY COLUMN html_description BLOB;
-ALTER TABLE restaurant_version MODIFY COLUMN html_description TEXT CHARACTER SET utf8;
+ALTER TABLE restaurant_version
+    MODIFY COLUMN html_description TEXT CHARACTER SET utf8;
 
 
 SET NAMES utf8;
 
-update menu_item set name = replace(name,unhex('C3A2E282ACE2809D'), '-') WHERE name REGEXP UNHEX('C3A2E282ACE2809D');
-update menu_item set name = replace(name,unhex('C3A2E282ACC593'), '"') WHERE name REGEXP UNHEX('C3A2E282ACC593');
-update menu_item set name = replace(name,unhex('C3A2E282ACC29D'), '"') WHERE name REGEXP UNHEX('C3A2E282ACC29D');
+update menu_item set name = replace(name,unhex('C3A2E282ACE2809D'), '-')
+    WHERE name REGEXP UNHEX('C3A2E282ACE2809D');
+update menu_item set name = replace(name,unhex('C3A2E282ACC593'), '"')
+    WHERE name REGEXP UNHEX('C3A2E282ACC593');
+update menu_item set name = replace(name,unhex('C3A2E282ACC29D'), '"')
+    WHERE name REGEXP UNHEX('C3A2E282ACC29D');
 
-update menuitem_version set html_description = replace(html_description,unhex('C3A2E282ACE2809D'), '-') WHERE html_description REGEXP UNHEX('C3A2E282ACE2809D');
-update menuitem_version set html_description = replace(html_description,unhex('C3A2E282ACC593'), '"') WHERE html_description REGEXP UNHEX('C3A2E282ACC593');
-update menuitem_version set html_description = replace(html_description,unhex('C3A2E282ACC29D'), '"') WHERE html_description REGEXP UNHEX('C3A2E282ACC29D');
-update menuitem_version set html_description = replace(html_description,unhex('C3A2E282ACE284A2'), "'") WHERE html_description REGEXP UNHEX('C3A2E282ACE284A2');
+update menuitem_version
+    set html_description =
+        replace(html_description,unhex('C3A2E282ACE2809D'), '-')
+    WHERE html_description REGEXP UNHEX('C3A2E282ACE2809D');
+update menuitem_version
+    set html_description =
+        replace(html_description,unhex('C3A2E282ACC593'), '"')
+    WHERE html_description REGEXP UNHEX('C3A2E282ACC593');
+update menuitem_version
+    set html_description =
+        replace(html_description,unhex('C3A2E282ACC29D'), '"')
+    WHERE html_description REGEXP UNHEX('C3A2E282ACC29D');
+update menuitem_version
+    set html_description =
+        replace(html_description,unhex('C3A2E282ACE284A2'), "'")
+    WHERE html_description REGEXP UNHEX('C3A2E282ACE284A2');
 
+update menu_item
+set name=replace(name,'Ã»','û')   where name LIKE '%Ã»%';
 
+update menu_item
+set name=replace(name,'â€”','-') where name LIKE '%â€”%';
 
-update menu_item  set name=replace(name,'Ã»','û')   where name LIKE '%Ã»%';
+update restaurant
+set name=replace(name,'Ã»','û') where name LIKE '%Ã»%';
 
-update menu_item  set name=replace(name,'â€”','-')   where name LIKE '%â€”%';
+update menu_item
+set name=replace(name,'Ã©','é') where name LIKE '%Ã©%';
 
-update restaurant set name=replace(name,'Ã»','û')   where name LIKE '%Ã»%';
-update menu_item  set name=replace(name,'Ã©','é')   where name LIKE '%Ã©%';
-update restaurant set name=replace(name,'Ã©','é')   where name LIKE '%Ã©%';
-update menu_item  set name=replace(name,'Ã¨','è')   where name LIKE '%Ã¨%';
-update restaurant set name=replace(name,'Ã¨','è')   where name LIKE '%Ã¨%';
-update menu_item  set name=replace(name, 'Ã¶', 'ö') where name LIKE '%Ã¶%';
-update restaurant set name=replace(name, 'Ã¶', 'ö') where name LIKE '%Ã¶%';
-update menu_item  set name=replace(name, 'Ã±', 'ñ')     where name LIKE '%Ã±%';
-update menu_item  set name=replace(name, 'â€™', "'")               where name like '%â€™%';
-update restaurant set name=replace(name, 'â€™', "'")               where name like '%â€™%';
-update restaurant set name=replace(name, 'Ã¼', 'ü')                 where name like '%Ã¼%';
-update restaurant set name=replace(name, 'TequilerÃ', 'Tequilerí')  where name LIKE '%TequilerÃ%';
-update restaurant set name=replace(name, 'TaquerÃ', 'Taquerí')      where name LIKE '%TaquerÃ%';
+update restaurant set name=replace(name,'Ã©','é') where name LIKE '%Ã©%';
+
+update menu_item  set name=replace(name,'Ã¨','è') where name LIKE '%Ã¨%';
+
+update restaurant set name=replace(name,'Ã¨','è') where name LIKE '%Ã¨%';
+
+update menu_item
+set name=replace(name, 'Ã¶', 'ö') where name LIKE '%Ã¶%';
+
+update restaurant
+set name=replace(name, 'Ã¶', 'ö') where name LIKE '%Ã¶%';
+
+update menu_item
+set name=replace(name, 'Ã±', 'ñ') where name LIKE '%Ã±%';
+
+update menu_item  set name=replace(name, 'â€™', "'")
+    where name like '%â€™%';
+update restaurant set name=replace(name, 'â€™', "'")
+    where name like '%â€™%';
+update restaurant set name=replace(name, 'Ã¼', 'ü')
+    where name like '%Ã¼%';
+update restaurant set name=replace(name, 'TequilerÃ', 'Tequilerí')
+    where name LIKE '%TequilerÃ%';
+update restaurant set name=replace(name, 'TaquerÃ', 'Taquerí')
+    where name LIKE '%TaquerÃ%';
 
 CREATE TABLE restaurant_tmp (
   id int(11) NOT NULL auto_increment,
@@ -178,17 +233,20 @@ CREATE TABLE restaurant_tmp (
   KEY version_id (version_id),
   UNIQUE name (name),
   UNIQUE slug (stripped_title),
-  CONSTRAINT FOREIGN KEY (version_id) REFERENCES restaurant_version (id) ON DELETE SET NULL
+  CONSTRAINT FOREIGN KEY (version_id)
+    REFERENCES restaurant_version (id) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT IGNORE restaurant_tmp SELECT * FROM restaurant;
 DROP TABLE restaurant;
 RENAME TABLE restaurant_tmp TO restaurant;
 
-update restaurant_version set description=replace(description,'â€', '"'), html_description=replace(html_description,'â€œ', '"')
-  where description like '%â€%';
+update restaurant_version set description=replace(description,'â€', '"'),
+    html_description=replace(html_description,'â€œ', '"')
+    where description like '%â€%';
 
-DELETE l FROM location l LEFT JOIN restaurant r ON l.restaurant_id=r.id WHERE r.id IS NULL;
+DELETE l FROM location l LEFT JOIN restaurant r ON l.restaurant_id=r.id
+    WHERE r.id IS NULL;
 DELETE FROM location WHERE stripped_title='';
 
 CREATE TABLE location_tmp (
@@ -214,7 +272,8 @@ CREATE TABLE location_tmp (
   UNIQUE KEY (restaurant_id,address,city,state,zip,country_id),
   KEY  (restaurant_id),
   KEY  (country_id),
-  CONSTRAINT  FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
+  CONSTRAINT FOREIGN KEY (restaurant_id)
+    REFERENCES restaurant (id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT IGNORE location_tmp SELECT * FROM location;
@@ -237,7 +296,9 @@ SET foreign_key_checks = 1;
         print counter, "of", len(locations)
         print location.restaurant_id, location.stripped_title
         print "Checking for dupes"
-        matches = Location.objects.filter(restaurant = location.restaurant, stripped_title = location.stripped_title)
+        matches = Location.objects.filter(restaurant = location.restaurant,
+        stripped_title = location.stripped_title)
+
         if len(matches) > 1:
             new_slug = location.generate_slug()
             print "Setting slug to: %s" %new_slug
@@ -245,11 +306,14 @@ SET foreign_key_checks = 1;
             location.save()
         counter += 1
 
-    print "SAFE to do the following: '%s'" % "ALTER TABLE location ADD constraint unique (restaurant_id, stripped_title)"
+    print "SAFE to do the following: '%s'" \
+    % "ALTER TABLE location "
+    + "ADD constraint unique (restaurant_id, stripped_title)"
 
     mysql("""
     DELETE FROM location where id IN (857, 22314,22315);
-    ALTER TABLE location ADD constraint unique (restaurant_id, stripped_title)
+    ALTER TABLE location
+        ADD constraint unique (restaurant_id, stripped_title)
     """)
     print "done"
 
@@ -273,18 +337,18 @@ SET foreign_key_checks = 1;
 
     alter table menu_item_image drop data;
     alter table menu_item_image add column `image` varchar(240) NOT NULL;
-    update menu_item_image set image = CONCAT('images/menuitems/',md5sum, '.jpg');
+    update menu_item_image
+        set image = CONCAT('images/menuitems/',md5sum, '.jpg');
     alter table menu_item_image drop md5sum;
-
 
     """
     sql = """
 
     alter table menu_item_image drop data;
     alter table menu_item_image add column `image` varchar(240) NOT NULL;
-    update menu_item_image set image = CONCAT('images/menuitems/',md5sum, '.jpg');
+    update menu_item_image
+        set image = CONCAT('images/menuitems/',md5sum, '.jpg');
     alter table menu_item_image drop md5sum;
-
 
     """
     mysql(sql)
